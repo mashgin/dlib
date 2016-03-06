d3df1a8801fb58b60c6b34a0374c1377b543d07e
Abhinai Srivastava <abhinai@gmail.com>
2016-03-05 20:27:14 -0800

    Adding install.py

---
+++ install.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import sys
+import os
+from subprocess import call
+
+def execute(str):
+	print str
+	call(str.split())
+
+install_path = "/usr/local"
+x11_include_path = "/opt/X11/include"
+x11_lib_path = "/opt/X11/lib"
+
+if len(sys.argv) == 2:
+	if (sys.argv[1] == "help"):
+		print "install.py = build and install"
+		print "install.py clean = uninstall"
+	elif (sys.argv[1] == "clean"):
+		execute("rm -rf " + install_path + "/include/dlib")
+		execute("rm -rf " + install_path + "/lib/libdlib.a")
+		if os.path.exists("build"):
+			execute("rm -rf build")
+	else:
+		print "Command not recognized"
+	sys.exit(0)
+
+if not os.path.exists("dlib"):
+	print "Please run this inside dlib dir"
+	sys.exit(0)
+
+if os.path.exists("build"):
+	execute("rm -rf build")
+
+execute("mkdir build")
+os.chdir("build")
+
+execute("cp -R ../dlib .")
+execute("g++ -O3 -c -I" + x11_include_path + " ./dlib/all/source.cpp -o source.o")
+execute("ar cr libdlib.a source.o")
+execute("cp -R dlib " + install_path + "/include")
+execute("cp libdlib.a " + install_path + "/lib")
